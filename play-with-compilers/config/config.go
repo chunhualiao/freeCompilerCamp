1eb7678ba507593f975c65708bb4cfb7caf30fba
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2019-06-30 09:07:55 -0700

    Update sandbox docker image to fully support LLVM and ROSE.
    In the sandbox, LLVM and ROSE have been compiled and installed. User can start to use directly.

--- play-with-compilers/config/config.go
+++ play-with-compilers/config/config.go
@@ -55,7 +55,7 @@ func ParseFlags() {
 	flag.StringVar(&CookieHashKey, "cookie-hash-key", "", "Hash key to use to validate cookies")
 	flag.StringVar(&CookieBlockKey, "cookie-block-key", "", "Block key to use to encrypt cookies")
 
-    flag.StringVar(&PlaygroundDomain, "playground-domain", "lab.freecompilercamp.org", "Domain to use for the playground")
+	flag.StringVar(&PlaygroundDomain, "playground-domain", "lab.freecompilercamp.org", "Domain to use for the playground")
 	flag.StringVar(&AdminToken, "admin-token", "", "Token to validate admin user for admin endpoints")
 
 	flag.StringVar(&SegmentId, "segment-id", "", "Segment id to post metrics")
